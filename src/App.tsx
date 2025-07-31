5bbcfe09d22d33479967d126d118f864b1abc207
FifthGit <obaadewale66@gmail.com>
2025-07-31 10:44:22 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-@@ .. @@
- import React, { useState, useEffect } from 'react'
- import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-+import { Toaster } from 'react-hot-toast'
- import { AuthProvider } from './contexts/AuthContext'
- import { ThemeProvider } from './contexts/ThemeContext'
- import LoadingScreen from './components/LoadingScreen'
-@@ .. @@
-           </Routes>
-         </Router>
-       </AuthProvider>
-+      <Toaster position="top-right" />
-     </ThemeProvider>
-   )
- }
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './contexts/AuthContext'
+import { ThemeProvider } from './contexts/ThemeContext'
+import LoadingScreen from './components/LoadingScreen'
+
+function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) {
+    return <LoadingScreen />
+  }
+
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<div>Home</div>} />
+            <Route path="/about" element={<div>About</div>} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+      <Toaster position="top-right" />
+    </ThemeProvider>
+  )
+}
+
+export default App
